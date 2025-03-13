ed04585bcf2d08236ca8b711b8b6aece21dae644
gabriel.lennon <gabriel.lennon@ifood.com.br>
2025-03-13 11:06:03 -0300

    feature: add pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 import "./globals.css";
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -24,6 +25,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '945446757776833');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=945446757776833&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
+      </head>
       <body className={`${inter.className} bg-[#050A30]`}>{children}</body>
     </html>
   );
